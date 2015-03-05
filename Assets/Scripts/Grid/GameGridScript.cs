f227d8dafad0d5937d307521b36de5de9981a34f
Jack Kelly <jackjkelly1506@gmail.com>
2015-03-05 23:24:58 +0000

    Setting up grid logic
    Generics! Abstracts! Things!

---
+++ Assets/Scripts/Grid/GameGridScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGridScript : MonoBehaviour {
+	public abstract class GameGrid<type>
+	{
+		private Hashtable grid;
+	}
+
+	public class KeyPair
+	{
+		public int x;
+		public int y;
+
+		public KeyPair(int xIn, int yIn)
+		{
+			x = xIn;
+			y = yIn;
+		}
+	}
+}
---
+++ Assets/Scripts/Grid/TileGridScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileGridScript : MonoBehaviour {
+	public class TileGrid:GameGridScript.GameGrid<Object>
+	{
+		private Hashtable grid;
+		int sizeX, sizeY;
+
+
+
+		//Constructor
+		public TileGrid()
+		{
+			grid = new Hashtable();
+		}
+
+
+
+		//Add/set tile at position
+		public void set(int x, int y, Object obj)
+		{
+			//update size bounds
+			if (x > sizeX)
+				sizeX = x;
+			if (y > sizeY)
+				sizeY = y;
+
+			//Add to hashtable
+			GameGridScript.KeyPair key = new GameGridScript.KeyPair (x, y);
+
+			set (key, obj);
+		}
+		public void set(GameGridScript.KeyPair key, Object obj)
+		{
+			grid.Add (key, obj);
+		}
+
+
+
+		//Get a tile
+		public Object get(int x, int y)
+		{
+			GameGridScript.KeyPair key = new GameGridScript.KeyPair (x, y);
+			return get(key);
+		}
+		public Object get(GameGridScript.KeyPair key)
+		{
+			return (Object)grid[key];
+		}
+	}
+}
